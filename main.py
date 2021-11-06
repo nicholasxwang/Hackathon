fc82df3dba7ac80f4ddb1ba331cdd98c3f14638d
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2021-11-06 23:38:29 +0000

    update

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ app = Flask('app')
 def index():
   return render_template("index.html")
 
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+@app.route("/dashboard")
+def dashboard():
+  return render_template('dashboard.html')
 app.run(host='0.0.0.0', port=8080)

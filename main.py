61e13a0a247b2f20c6cf7054772dbb58349038ae
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2021-11-06 23:13:36 +0000

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 app = Flask('app')
 
 @app.route('/')
-def hello_world():
+def index():
   return render_template("index.html")
 
 app.run(host='0.0.0.0', port=8080)

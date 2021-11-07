c240dd293500951caf2b9774abea7fbc17d657ad
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2021-11-07 00:01:29 +0000

    update

--- static/nodejs/maze.js
+++ static/nodejs/maze.js
@@ -1,1 +1,15 @@
-var canvas 
+var canvas = document.getElementById("maze")
+var context = canva.getContext("2d");
+var rectangleX = 450;
+var rectangleY = 5
+var width = 560;
+var height = 560;
+var interval;
+function createMaze(rx,ry)
+{
+  makeWhite(0,0, canvas.width,canvas.height);
+  var photo = new Image();
+  photo.onload = function(){
+    
+  }
+}

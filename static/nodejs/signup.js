d38df4e3d0e6a33c778e9501dd6a09f58633c169
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2021-11-07 13:55:39 +0000

    update

--- static/nodejs/signup.js
+++ static/nodejs/signup.js
@@ -1,6 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
 
 function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -16,15 +13,19 @@ function validate(email) {
   }
 }
 
-$("#email").on("input", validate);
 
 window.onload = function() {  
-  var userInput = document.getElementById('email');
+  var userInput = document.getElementById('email').value;
     userInput.onkeyup = function() {
       var value = this.value;
       if (validate(value)){
         document.getElementById("bubble").style.display="block";
-        document.getElementById("bubble").style.background="rgba(26, 255, 0,0.3)";
+        document.getElementById("bubble2").style.display="none";
+        document.getElementById("bubble2").value = "Message: Valid Email!"
+      }else{
+        document.getElementById("bubble2").style.display="block";
+        document.getElementById("bubble").style.display="none";
+        document.getElementById("bubble2").value = "Message: Invalid Email!"
       }
 }
 }

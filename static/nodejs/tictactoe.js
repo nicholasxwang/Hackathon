7e68e8062c20202a04c5a3d3430876faae58a4fe
Kousthubh Veturi <93627848+Kousthubh-Veturi@users.noreply.github.com>
2021-11-07 10:06:27 +0000

    update

--- static/nodejs/tictactoe.js
+++ static/nodejs/tictactoe.js
@@ -1,11 +1,11 @@
 const postStatus document.querySelector('.status');
 
-let active = true;
-let player = "x";
 let state = ["", "", "", "", "", "", "", "", ""];
-const winningMessage = () => "Player ${currentPlayer} wins!";
-const drawMessage = () => "Draw!";
-const currentPlayerTurn = () => "It's ${currentPlayer}'s turn";
+let player = "x";
+let active = true;
+const win = "Winner is: " + player;
+const drawMessage = "Draw!";
+const currentPlayerTurn = "Turn: " + player;
 statusDisplay.innerHTML = currentPlayerTurn();
 /* 
 0 1 2
@@ -42,13 +42,13 @@ function result(){
   let win = false;
   for(int i = 0; i<=7; i++){
     const wincondt = winconds[i];
-    let a = state[winCondition[0]];
-    let b = state[winCondition[1]];
-    let c = state[winCondition[2]];
+    let a = state[wincondt[0]];
+    let b = state[wincondt[1]];
+    let c = state[wincondt[2]];
     if (a === "" || b === "" || c === "") {
       continue;
     }
-    if (a === b && b === c) {
+    if (a === b === c) {
       win = true;
       break;
     }
@@ -58,4 +58,17 @@ function result(){
     active = false;
     return;
   }
+  var draw = !state.includes("");
+  if (win == true){
+    statusDisplay.innerHTML = drawMessage();
+    gameActive = false;
+    return;
+  
+  }
+  handlePlayer();
+  
+}
+function click(clicked){
+  const pressed_cell = clicked.target;
+  
 }

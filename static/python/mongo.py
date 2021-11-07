a41ae5c22cce810418f3cdff4f7cc04af8fb78f1
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2021-11-07 02:51:49 +0000

    update

--- static/python/mongo.py
+++ static/python/mongo.py
@@ -1,1 +1,26 @@
 import pymongo
+import os
+from weurkzeug.security import generate_password_hash, check_password_hash
+
+def get_user(email):
+  db = pymongo.MongoClient(os.environ['token']).Users.Users.find({})
+  dictionary = {}
+  for i in db:
+    try:
+      dictionary["email"]=i["email"]
+    except:
+      dictionary["email"] = None
+    try:
+      dictionary["username"]=i["username"]
+    except:
+      dictionary["username"] = None
+    try:
+      dictionary["password"]=generate_password_hash(i["password"])
+    except:
+      dictionary["password"] = None
+    try:
+      dictionary["username"]=i["username"]
+    except:
+      dictionary["username"] = None
+
+    

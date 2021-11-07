bcae3cfc34874b208aae7f9b72b0bc4df66d12f2
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2021-11-07 13:51:09 +0000

    update

--- static/nodejs/signup.js
+++ static/nodejs/signup.js
@@ -1,5 +1,30 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validate(email) {
+
+  if (validateEmail(email)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+$("#email").on("input", validate);
+
 window.onload = function() {  
   var userInput = document.getElementById('email');
     userInput.onkeyup = function() {
       var value = this.value;
+      if (validate(value)){
+        document.getElementById("bubble").style.display="block";
+        document.getElementById("bubble").style.background="rgba(26, 255, 0,0.3)";
+      }
+}
 }

e5b16dfbb40d71bbc53026c96b2aa572c9ace5eb
Kousthubh Veturi <93627848+Kousthubh-Veturi@users.noreply.github.com>
2021-11-07 05:36:48 +0000

    update

---
+++ static/nodejs/wordgame.js
@@ -0,0 +1,2 @@
+var wordList = ["halloween", "pumpkin", "decoration", "vampire", "zombie", "haunted", "costume", "october", "autumn", "graveyard", "broomstick", "eerie", "midnight", "spooky", "spiders", "ominous", "shadow", "skeleton", "supernatural", "werewolf", "lantern", "magical", "forest", "treasure", "trickery", "chocolate", "cobwebs"];
+var word = wordList[Math.floor(Math.random() * 27)];
